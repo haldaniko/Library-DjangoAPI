a473777a8f56e01fd3eecc175f1c73400f71b205
haldaniko <haldaniko@gmail.com>
2024-07-31 20:34:10 +0200

    add AUTH_USER_MODEL in settings.py; register "user" in INSTALLED_APPS

--- library/settings.py
+++ library/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    "books_service"
+    "books_service",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -117,3 +118,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user.Customer'
