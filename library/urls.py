95b7521267857e1df317db33419abb1411b21cf2
haldaniko <haldaniko@gmail.com>
2024-07-31 21:05:42 +0200

    include 'user.urls'

--- library/urls.py
+++ library/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", include("books_service.urls"), name="books_service")
+    path("api/", include("books_service.urls"), name="books_service"),
+    path("api/user/", include("user.urls", namespace="user")),
 ]
