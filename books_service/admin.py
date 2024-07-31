8faafaa8f43c690c146a62df570683638e24c533
haldaniko <haldaniko@gmail.com>
2024-07-31 19:31:35 +0200

    register 'Book' model in admin.py

--- books_service/admin.py
+++ books_service/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'cover', 'inventory', 'daily_fee')
