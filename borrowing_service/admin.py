6c0b8c54a5be765d8eb54eb6cbdad38106f19ce5
haldaniko <haldaniko@gmail.com>
2024-08-01 15:43:15 +0200

    register 'Borrowing' and 'Payment' in admin.py

--- borrowing_service/admin.py
+++ borrowing_service/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Borrowing, Payment
 
-# Register your models here.
+
+@admin.register(Borrowing)
+class BorrowingAdmin(admin.ModelAdmin):
+    list_display = ('borrow_date', 'expected_return_date', 'actual_return_date', 'book', 'user')
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('status', 'payment_type', 'borrowing', 'session_url', 'session_id', 'money_to_pay')
