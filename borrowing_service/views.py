528fe6134904c358406ac469a39811d3d6fc3096
haldaniko <haldaniko@gmail.com>
2024-08-01 18:12:10 +0200

    add filter logic for 'BorrowingViewSet'

--- borrowing_service/views.py
+++ borrowing_service/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from .models import Payment, Borrowing
-from .serializers import PaymentSerializer, BorrowingSerializer
+from .models import Borrowing, Payment
+from .serializers import BorrowingSerializer, PaymentSerializer
 
 
 class PaymentViewSet(viewsets.ModelViewSet):
@@ -11,3 +11,16 @@ class PaymentViewSet(viewsets.ModelViewSet):
 class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+
+    def get_queryset(self):
+        user_id = self.request.query_params.get('user_id')
+        is_active = self.request.query_params.get('is_active')
+
+        queryset = self.queryset
+
+        if user_id:
+            queryset = queryset.filter(user_id=user_id)
+        if is_active:
+            queryset = queryset.filter(is_active=is_active.lower() == 'true')
+
+        return queryset.distinct()
