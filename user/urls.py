ed7b70482bcefc5fee8b21ad167f3fbf16116932
haldaniko <haldaniko@gmail.com>
2024-08-02 17:06:41 +0200

    remove CreateTokenView endpoint

--- user/urls.py
+++ user/urls.py
@@ -7,7 +7,6 @@ app_name = "user"
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
-    path("login/", CreateTokenView.as_view(), name="login"),
     path("me/", ManageUserView.as_view(), name="manage"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
