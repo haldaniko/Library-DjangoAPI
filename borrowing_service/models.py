70fec85028d29be30e4bec4f9a6f8781df1757fe
haldaniko <haldaniko@gmail.com>
2024-08-02 17:13:47 +0200

    replace Customer with User

--- borrowing_service/models.py
+++ borrowing_service/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from django.core.validators import MinValueValidator
 
 from books_service.models import Book
-from user.models import Customer
+from user.models import User
 
 
 class Borrowing(models.Model):
@@ -13,7 +13,7 @@ class Borrowing(models.Model):
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(null=True, blank=True)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    user = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.user} borrowed {self.book} on {self.borrow_date}"
