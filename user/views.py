bdce02f8426f81d5f799ed937bcafd42fa14fa73
haldaniko <haldaniko@gmail.com>
2024-08-02 17:33:28 +0200

    change authentication_classes in ManageUserView

--- user/views.py
+++ user/views.py
@@ -1,8 +1,8 @@
 from rest_framework import generics
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from user.serializers import UserSerializer, AuthTokenSerializer
 
@@ -18,7 +18,7 @@ class CreateTokenView(ObtainAuthToken):
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
-    authentication_classes = (TokenAuthentication,)
+    authentication_classes = (JWTAuthentication,)
     permission_classes = (IsAuthenticated,)
 
     def get_object(self):
