86c97f5931a8f8217b3558e7a9de4fb26556519d
haldaniko <haldaniko@gmail.com>
2024-08-02 18:50:38 +0200

    add authentication and permission logic

--- books_service/views.py
+++ books_service/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from permissions import IsAdminOrReadOnly
 from .models import Book
 from .serializers import BookSerializer
 
@@ -6,3 +9,5 @@ from .serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAdminOrReadOnly,)
