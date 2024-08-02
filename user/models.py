34bbebe5dd1e709940e75c1a7de29a87ecb8d8f8
haldaniko <haldaniko@gmail.com>
2024-08-02 16:47:38 +0200

    update User model

--- user/models.py
+++ user/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.utils.translation import gettext as _
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.models import (
+    AbstractUser,
+    UserManager as DjangoUserManager
+)
 
 
-class UserManager(BaseUserManager):
+class UserManager(DjangoUserManager):
     """Define a model manager for User model with no username field."""
 
     use_in_migrations = True
@@ -22,7 +25,11 @@ class UserManager(BaseUserManager):
         """Create and save a regular User with the given email and password."""
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(
+            email=email,
+            password=password,
+            **extra_fields
+        )
 
     def create_superuser(self, email, password, **extra_fields):
         """Create and save a SuperUser with the given email and password."""
@@ -34,28 +41,23 @@ class UserManager(BaseUserManager):
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
 
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(
+            email=email, password=password,
+            **extra_fields
+        )
 
 
-class Customer(AbstractUser):
+class User(AbstractUser):
+    """User model."""
     username = None
     email = models.EmailField(_("email address"), unique=True)
-
-    groups = models.ManyToManyField(
-        "auth.Group",
-        verbose_name=_("groups"),
-        blank=True,
-        related_name="custom_user_set",
-    )
-    user_permissions = models.ManyToManyField(
-        "auth.Permission",
-        verbose_name=_("user permissions"),
-        blank=True,
-        related_name="custom_user_permissions_set",
-    )
+    first_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=15)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ['first_name', 'last_name']
+    REQUIRED_FIELDS = []
 
     objects = UserManager()
 
