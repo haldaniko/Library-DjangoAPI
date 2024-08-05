c7331e29e55df2f7bc796b77497844eba7dc883c
haldaniko <haldaniko@gmail.com>
2024-08-05 14:41:34 +0200

    repair 'permissions' import

--- books_service/views.py
+++ books_service/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from permissions import IsAdminOrReadOnly
+from .permissions import IsAdminOrReadOnly
 from .models import Book
 from .serializers import BookSerializer
 
